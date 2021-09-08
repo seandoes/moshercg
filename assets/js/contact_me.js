87e60a0854627f3d1e49b08f2fd3cd4645a4c159
Sean <sdoesimkins@gmail.com>
2021-09-08 10:48:21 -0500

    Add temporary handling for 302s

--- assets/js/contact_me.js
+++ assets/js/contact_me.js
@@ -46,7 +46,21 @@ $(function() {
           $('#contactForm').trigger("reset");
         },
 		
-        error: function() {
+        error: function(error) {
+          // TODO this is brittle, fix this
+          if (error.status == 0) {
+            // Success message
+            $('#success').html("<div class='alert alert-success'>");
+            $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
+              .append("</button>");
+            $('#success > .alert-success')
+              .append("<strong>Your message has been sent. </strong>");
+            $('#success > .alert-success')
+              .append('</div>');
+            //clear all fields
+            $('#contactForm').trigger("reset");
+            return;
+          }
           // Fail message
           $('#success').html("<div class='alert alert-danger'>");
           $('#success > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
